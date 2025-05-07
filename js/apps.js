cf85a49cf9a2a0ac4040f8a44ca0fdd50ea4ac29
Sayanmagar01 <Sayanmagar01@gmail.com>
2025-05-07 09:51:49 -0500

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,130 @@
+versions.v1 = {
+  name: "Pissè",
+  version: "1.0",
+  date: "2025",
+  folder: "asset-v1/",
+  looptime: 7105,
+  bpm: 135,
+  totalframe: 340,
+  nbpolo: 7,
+  bonusloopA: false, 
+  bonusendloopA: true,
+  colBck: "#01245C",
+  col0: "#00ffff",
+  col1: "#00bcbc",
+  col2: "#008a8a",
+  col3: "#005555",
+  col4: "#005555",
+  animearray: [
+    {
+      name: "1_kickingtheassesofthehaters",
+      color: "328050",
+      uniqsnd: !0, 
+    },
+    {
+      name: "2_clara",
+      color: "328050",
+      uniqsnd: !0,
+    },
+    {
+      name: "3_clockette",
+      color: "328050",
+      uniqsnd: !1,
+    },
+    {
+      name: "4_pissbox",
+      color: "328050",
+      uniqsnd: !0,
+    },
+    {
+      name: "5_silly_drummer",
+      color: "328050",
+      uniqsnd: !1,
+    },
+    {
+      name: "6_kubonee",
+      color: "58B69B",
+      uniqsnd: !1,
+    },
+    {
+      name: "7_allyxa",
+      color: "58B69B",
+      uniqsnd: !0,
+    },
+    {
+      name: "8_alicia",
+      color: "58B69B",
+      uniqsnd: !0,
+    },
+    {
+      name: "9_saro",
+      color: "58B69B",
+      uniqsnd: !1,
+    },
+    {
+      name: "10_sabine",
+      color: "58B69B",
+      uniqsnd: !0,
+    },
+    {
+      name: "11_krystal",
+      color: "556CC9",
+      uniqsnd: !1,
+    },
+    {
+      name: "12_beware_the_pipeline",
+      color: "556CC9",
+      uniqsnd: !1,
+    },
+    {
+      name: "13_veldie",
+      color: "556CC9",
+      uniqsnd: !1,
+    },
+    {
+      name: "14_exterminate",
+      color: "556CC9",
+      uniqsnd: !1,
+    },
+    {
+      name: "15_pixie",
+      color: "556CC9",
+      uniqsnd: !1,
+    },
+    {
+      name: "16_chester",
+      color: "01A2C5",
+      uniqsnd: !1,
+    },
+    {
+      name: "17_maxwell",
+      color: "01A2C5",
+      uniqsnd: !1,
+    },
+    {
+      name: "18_fappin",
+      color: "01A2C5",
+      uniqsnd: !1,
+    },
+    {
+      name: "19_suckin",
+      color: "01A2C5",
+      uniqsnd: !1,
+    },
+    {
+      name: "20_drake",
+      color: "01A2C5",
+      uniqsnd: !1,
+    },
+  ],
+  bonusarray: [
+    // no bonus
+  ],
+
+};
+
+// versions.v2 = {
+//  name: "New Version",
+// version: "2",
+// date: "2025",
+// ...
